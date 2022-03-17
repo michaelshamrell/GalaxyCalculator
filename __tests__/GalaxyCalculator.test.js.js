ddd22108a0896e9a7c0bbfc132cbf15e1568969e
michaelshamrell <michaelshamrell2001@gmail.com>
2022-03-16 17:53:26 -0700

    Test venusCalc

--- __tests__/GalaxyCalculator.test.js.js
+++ __tests__/GalaxyCalculator.test.js.js
@@ -6,6 +6,7 @@ describe('Galaxy Calculator', () => {
     const GalaxyCalculatorInstance = new GalaxyCalculator(20);
     expect(GalaxyCalculatorInstance.marsCalc()).toEqual("Your mars age is 10.638297872340425 and you have 67.36170212765957 years left to live"); 
     expect(GalaxyCalculatorInstance.mercuryCalc()).toEqual("your mercury age is 83.33333333333334 You have lived 5.333333333333343 years past life expenctancy"); 
-    expect(GalaxyCalculatorInstance.venusCalc()).toEqual("your mercury age is 83.33333333333334 You have lived 5.333333333333343 years past life expenctancy"); 
+    expect(GalaxyCalculatorInstance.venusCalc()).toEqual("Your venus age is 32.25806451612903 and you have 45.74193548387097 years left to live"); 
+    expect(GalaxyCalculatorInstance.jupiterCalc()).toEqual("Your jupiter age is 1.6863406408094437 and you have 76.31365935919055 years left to live"); 
   });
 });
