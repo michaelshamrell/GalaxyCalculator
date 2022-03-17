d4487997f7b45f571fb2fb199bf174ca2ee5799c
michaelshamrell <michaelshamrell2001@gmail.com>
2022-03-16 17:52:09 -0700

    Tets mercuryCalc()

--- __tests__/GalaxyCalculator.test.js.js
+++ __tests__/GalaxyCalculator.test.js.js
@@ -5,9 +5,7 @@ describe('Galaxy Calculator', () => {
   test('should correctly generate your age on given planets', () => {
     const GalaxyCalculatorInstance = new GalaxyCalculator(20);
     expect(GalaxyCalculatorInstance.marsCalc()).toEqual("Your mars age is 10.638297872340425 and you have 67.36170212765957 years left to live"); 
-  });
-  test('should correctly generate your age on given planets', () => {
-    const GalaxyCalculatorInstance = new GalaxyCalculator(20);
     expect(GalaxyCalculatorInstance.mercuryCalc()).toEqual("your mercury age is 83.33333333333334 You have lived 5.333333333333343 years past life expenctancy"); 
-  });c
+    expect(GalaxyCalculatorInstance.venusCalc()).toEqual("your mercury age is 83.33333333333334 You have lived 5.333333333333343 years past life expenctancy"); 
+  });
 });
